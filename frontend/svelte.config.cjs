8b54b6c414b0464fd1a19734cb959d138eac78ac
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2021-12-05 20:50:41 +0800

    Add Storybook

---
+++ frontend/svelte.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('@sveltejs/kit').Config} */
+module.exports = {
+  config: {
+    kit: {
+      // hydrate the <div id="svelte"> element in src/app.html
+      target: '#svelte',
+    },
+  },
+}
+
