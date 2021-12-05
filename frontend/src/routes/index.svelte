082ded7b42b0e51f8f3af318ea8d61c4d4a2ac0d
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2021-12-05 20:39:40 +0800

    Added Tailwind, PostCSS & Stylelint support

--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
+<script>
+  import "../style/tailwind.css";
+</script>
+
+<h1 class="text-purple-400">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
