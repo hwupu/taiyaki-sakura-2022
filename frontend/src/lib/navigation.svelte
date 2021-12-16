fea380581df0865dfca272f07c9560a70e8cf23e
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2021-12-16 18:27:46 +0800

    Seperate into AnimatedLogo

--- frontend/src/lib/navigation.svelte
+++ frontend/src/lib/navigation.svelte
@@ -2,68 +2,7 @@
 	<ul class="flex items-end gap-4 text-center">
 		<li class="flex-none order-3 w-[238px] mx-[calc(-1rem-5pt)]">
 			<a href="#">
-				<svg width="238" height="233">
-					<foreignObject x="0" y="0" width="238" height="231" style="filter: grayscale(100%);">
-						<img
-							src="images/png/taiyaki-type-ring@3x.png"
-							alt="Logo of Taiyaki Sakura"
-							width="238"
-							height="231"
-						>
-					</foreignObject>
-					<foreignObject x="0" y="0" width="208" height="139" transform="translate(27.5 49.5) rotate(0 104 139)">
-				    <picture>
-				    	<source srcset="images/svg/taiyaki.svg" type="image/svg+xml">
-				    	<source srcset="images/webp/taiyaki.webp, images/webp/taiyaki@2x.webp 2x, images/webp/taiyaki@3x.webp 3x" type="image/webp">
-							<img
-								src="images/png/taiyaki.png"
-								alt="Logo of Taiyaki Sakura"
-								width="208"
-								height="139"
-								srcset="images/png/taiyaki@2x.png 2x, images/png/taiyaki@3x.png 3x"
-							>
-				    </picture>
-					</foreignObject>
-					<foreignObject x="0" y="0" width="46" height="43" transform="translate(183 143.7) rotate(21.5 23 21.5)">
-				    <picture>
-				    	<source srcset="images/svg/sakura-flat.svg" type="image/svg+xml">
-				    	<source srcset="images/webp/sakura-flat.webp, images/webp/sakura-flat@2x.webp 2x, images/webp/sakura-flat@3x.webp 3x" type="image/webp">
-							<img
-								src="images/png/sakura-flat.png"
-								alt="Logo of Taiyaki Sakura"
-								width="46"
-								height="43"
-								srcset="images/png/sakura-flat@2x.png 2x, images/png/sakura-flat@3x.png 3x"
-							>
-				    </picture>
-					</foreignObject>
-					<foreignObject x="0" y="0" width="46" height="43" transform="translate(23 187.5) rotate(-20 23 21.5)">
-				    <picture>
-				    	<source srcset="images/svg/sakura-flat.svg" type="image/svg+xml">
-				    	<source srcset="images/webp/sakura-flat.webp, images/webp/sakura-flat@2x.webp 2x, images/webp/sakura-flat@3x.webp 3x" type="image/webp">
-							<img
-								src="images/png/sakura-flat.png"
-								alt="Logo of Taiyaki Sakura"
-								width="46"
-								height="43"
-								srcset="images/png/sakura-flat@2x.png 2x, images/png/sakura-flat@3x.png 3x"
-							>
-				    </picture>
-					</foreignObject>
-					<foreignObject x="31.5" y="162.5" width="175" height="69">
-				    <picture>
-				    	<source srcset="images/svg/type-logo.svg" type="image/svg+xml">
-				    	<source srcset="images/webp/type-logo.webp, images/webp/type-logo@2x.webp 2x, images/webp/type-logo@3x.webp 3x" type="image/webp">
-							<img
-								src="images/png/type-logo.png"
-								alt="Logo of Taiyaki Sakura"
-								width="175"
-								height="69"
-								srcset="images/png/type-logo@2x.png 2x, images/png/type-logo@3x.png 3x"
-							>
-				    </picture>
-					</foreignObject>
-				</svg>
+				<AnimatedLogo />
 				<span class="sr-only">Home</span>
 			</a>
 		</li>
@@ -73,3 +12,7 @@
 		<li class="bg-red-400 flex-1 order-5"><a href="#">Link4</a></li>
 	</ul>
 </nav>
+
+<script>
+	import AnimatedLogo from "$lib/animated-logo.svelte";
+</script>
