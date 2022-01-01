61994be43e57f7e6fdf44bfcc491a3d70381c5b7
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2022-01-01 14:04:22 +0800

    Fix cros

--- backend/cms/settings/production.py
+++ backend/cms/settings/production.py
@@ -12,7 +12,8 @@ ALLOWED_HOSTS = [os.environ.get('HOSTNAME')]
 # CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ORIGIN_WHITELIST = (
-  os.environ.get('HOSTNAME'),
+  'http://'+os.environ.get('HOSTNAME'),
+  'http://localhost:3000',
   'http://localhost:8000',
 )
 
