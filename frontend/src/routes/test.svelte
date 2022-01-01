f3b6322c7ecf1a28125967997ca8e1e148fcbd7a
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2022-01-01 14:52:01 +0800

    Idk howto

--- frontend/src/routes/test.svelte
+++ frontend/src/routes/test.svelte
@@ -1,38 +1,25 @@
+<script context="module">
+	export async function load() {
+		const result = await fetch('https://sakura.phwu.work/api/v2/images')
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+				return data;
+			})
+			.catch((error) => {
+				console.log(error);
+				return 'error';
+			});
+		return { props: { result } };
+	}
+</script>
+
 <script>
-	let result = undefined;
-	let result2 = undefined;
-
-	fetch('http://backend:8000/api/v2/images')
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-			result = data;
-		})
-		.catch((error) => {
-			console.log(error);
-			return [];
-		});
-
-	fetch('http://localhost:8000/api/v2/images')
-		.then((response) => response.json())
-		.then((data) => {
-			console.log(data);
-			result2 = data;
-		})
-		.catch((error) => {
-			console.log(error);
-			return [];
-		});
+	export let result;
 </script>
 
 {#if result}
 	<div>
 		bakcend: {result.meta.total_count}
-	</div>
-{/if}
-
-{#if result2}
-	<div>
-		localhost: {result2.meta.total_count}
 	</div>
 {/if}
