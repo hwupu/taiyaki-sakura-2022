8c62edac3f0de6195ef7163f2d53370c5bbdcd00
Pu-Hsuan Wu <hwupu.cs01@g2.nctu.edu.tw>
2022-01-03 02:24:16 +0800

    Change to React / Nextjs

--- frontend/src/hooks.js
+++
@@ -1,5 +0,0 @@
-export function getSession(request) {
-	let acceptedLanguage =
-		request.headers['accept-language'] && request.headers['accept-language'].split(',')[0];
-	return { acceptedLanguage };
-}
